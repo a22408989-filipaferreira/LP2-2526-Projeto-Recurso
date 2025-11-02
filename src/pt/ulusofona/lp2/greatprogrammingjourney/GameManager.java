094d7ac918ac29364293877c4a6a4340c1cb8aaf
Beatriz <a22407398@alunos.ulht.pt>
2025-11-02 02:42:13 +0000

    Updates

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -2,17 +2,15 @@ package pt.ulusofona.lp2.greatprogrammingjourney;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class GameManager {
-    /* attributes */
     Board board;
     ArrayList<Player>players;
     int currentPlayerIndex;
     int turnNumber;
-
-    /*  */
-
-    /* constructors */
 
     public GameManager(){
         this.board = new Board();
@@ -32,22 +30,51 @@ public class GameManager {
 
     public void movePlayer(int numberOfSlots){}
 
+
     public boolean checkIfGameIsOver(){
         return false;
     }
 
-    /* methods */
 
-    public boolean createInitialBoard(String[][] playerInfo, int worldSize){
-Player p =
+    public String[] getProgrammerInfo(int id){
+        Player player = null;
+        for (Player p : players) {
+            if (p.getId() == id) {
+                player = p;
+                break;
+            }
+        }
+        if (player == null) {
+            return null;
+        }
 
-        p.getId()
+        List<String> langs = new ArrayList<>(player.getFavoriteLanguages());
+        Collections.sort(langs, String.CASE_INSENSITIVE_ORDER);
+
+        String langsStr = String.join("; ", langs);
+
+        String[] info = new String[5];
+        info[0] = String.valueOf(player.getId());
+        info[1] = player.getName();
+        info[2] = String.valueOf(player.getCurrentPosition());
+        info[3] = langsStr;
+        info[4] = player.getStatus().getDisplayName();
+
+        return info;
     }
 
 
+    public String getProgrammerInfoAsStr(int id){
+        String[] info = getProgrammerInfo(id);
+
+        if (info == null) {
+            return null;
+        }
+
+        return String.join(" | ", info);
+    }
+    //public boolean createInitialBoard(String[][] playerInfo, int worldSize){}
     //public String getImagePng(int nrSquare){}
-    //public String[] getProgrammerInfo(int id){}
-    //public String getProgrammerInfoAsStr(int id){}
     //public String[] getSlotInfo(int position){}
     //public int getCurrentPlayerID(){}
     //public boolean moveCurrentPlayer(int nrSpaces){}
